b6f1240727d5e13990d92110ba0e34625b167d58
Sean Sutherland <seansutherland@Seans-iMac.local>
2017-10-26 21:58:51 -0600

    Created a service to handle getting some mock data to the UI/App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,7 @@
 import { Component } from '@angular/core';
 import {Hero} from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import {HeroService} from './hero.service';
+import {OnInit} from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -76,18 +65,28 @@ const HEROES: Hero[] = [
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+providers:[HeroService]
 })
 
-export class AppComponent  {
+export class AppComponent implements OnInit  {
  title = 'Tour of Heroes';
+ heroes: Hero[];
+ selectedHero: Hero;
 
-	selectedHero: Hero;
+ constructor(private heroService: HeroService){}
 
- heroes = HEROES;
+ngOnInit(): void{
+		this.getHeroes();
+}
 
+ getHeroes(): void{
+	 this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+ }
 //getting called from the click event of the hero list item
  onSelect(hero: Hero): void {
  	this.selectedHero = hero;
  }
+
+
 }
